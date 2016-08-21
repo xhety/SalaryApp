ca6c9c837e4216c67cda4b990470fd285a9baff1
HetyTse <xhety@163.com>
2016-08-21 23:16:40 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.all('admin/staff', function (req, res,next) {
   if (req.user.isadmin==1) {
     res.render('admin/staff', {title: '人事信息管理',user:req.user});
   } else {
-    res.render('salary', {title: '工资信息查看',user:req.user});
+    res.render('admin/index', {title: '工资信息查看',user:req.user});
   }
   next();
 });
